ccc480970a358a0f9fd5a119308a25bc66147664
musfiqurrahmansafin <132481032+musfiqurrahmansafin@users.noreply.github.com>
2024-03-06 21:37:39 +0600

    Update CityManager.java

--- src/CityManager.java
+++ src/CityManager.java
@@ -2,52 +2,37 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-
 /**
  * Manages operations on cities, including adding, deleting, counting, and sorting.
  */
-public class CityManager {
-    private List<City> cityList = new ArrayList<>();
-
+public class CityManager{
+    private List<City>cityList=new ArrayList<>();
     /**
      * Deletes a city from the list. If the city does not exist, throws a NoSuchElementException.
      *
      * @param city the city to delete
      * @throws NoSuchElementException if the city does not exist in the list
      */
-    public void delete(City city) throws NoSuchElementException {
-        if (!cityList.remove(city)) {
-            throw new NoSuchElementException("City does not exist.");
-        }
+    public void delete(City city) throws NoSuchElementException{
+        if (!cityList.remove(city)) throw new NoSuchElementException("City does not exist.");
     }
-
     /**
      * Returns the total number of cities in the list.
      *
      * @return the total number of cities
      */
-    public int count() {
-        return cityList.size();
-    }
-
+    public int count(){return cityList.size();}
     /**
      * Returns a list of cities sorted based on the specified sortType.
      *
      * @param sortType the type of sorting: "name" for city name, "province" for province name
      * @return a sorted list of cities
      */
-    public List<City> getCities(String sortType) {
-        List<City> sortedList = new ArrayList<>(cityList);
-        if ("name".equalsIgnoreCase(sortType)) {
-            sortedList.sort(Comparator.comparing(City::getName));
-        } else if ("province".equalsIgnoreCase(sortType)) {
-            sortedList.sort(Comparator.comparing(City::getProvince));
-        }
+    public List<City>getCities(String sortType){
+        List<City>sortedList=new ArrayList<>(cityList);
+        if("name".equalsIgnoreCase(sortType)) sortedList.sort(Comparator.comparing(City::getName));
+        else if("province".equalsIgnoreCase(sortType)) sortedList.sort(Comparator.comparing(City::getProvince));
         return sortedList;
     }
-
-    // Method to add a city to the list, for completeness
-    public void addCity(City city) {
-        cityList.add(city);
-    }
+    public void addCity(City city){cityList.add(city);}
 }
